3e38292c25dfc928cac8aeb320657614e90f905f
Ronny Wang <ronnywang@gmail.com>
2019-04-13 15:21:28 +0800

    加上 NewsHour->getData() 並處理多餘的 section

--- webdata/models/NewsHour.php
+++ webdata/models/NewsHour.php
@@ -1,11 +1,24 @@
 <?php
 
+class NewsHourRow extends Pix_Table_Row
+{
+    public function getData()
+    {
+        $data = json_decode($this->data);
+        if (!$data->sections[count($data->sections) - 1]->end) {
+            arraY_pop($data->sections);
+        }
+        return $data;
+    }
+}
+
 class NewsHour extends Pix_Table
 {
     public function init()
     {
         $this->_name = 'news_hour';
         $this->_primary = array('channel', 'time');
+        $this->_rowClass = 'NewsHourRow';
 
         $this->_columns['channel'] = array('type' => 'varchar', 'size' => 8);
         $this->_columns['time'] = array('type' => 'int');
