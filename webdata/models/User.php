1e2ec3b6ff67b041eae923817c2e87b8a914cb8a
Ronny Wang <ronnywang@gmail.com>
2019-07-03 15:07:11 +0800

    update User model, add display_name and change user_id to char(9)

--- webdata/models/User.php
+++ webdata/models/User.php
@@ -8,10 +8,10 @@ class User extends Pix_Table
 
         $this->_primary = 'user_id';
 
-        $this->_columns['user_id'] = array('type' => 'int', 'auto_increment' => true);
+        $this->_columns['user_id'] = array('type' => 'char', 'size' => 9);
         $this->_columns['user_name'] = array('type' => 'varchar', 'size' => 255);
+        $this->_columns['display_name'] = array('type' => 'varchar', 'size' => 255);
 
         $this->addIndex('user_name', array('user_name'), 'unique');
-
     }
 }
